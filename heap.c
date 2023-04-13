f24acbeebb4bee808523eacf611376596a851768
Mateusz Milczarek <milczary.mm@gmail.com>
2023-04-13 22:03:08 +0200

    Added heap_setup function

--- heap.c
+++ heap.c
@@ -1,1 +1,14 @@
 #include "heap.h"
+
+struct memory_manager_t memory_manager;
+
+int heap_setup(void){
+    void *heap = sbrk(0);
+    if (!heap){
+        return -1;
+    }
+    memory_manager.memory_size = 0;
+    memory_manager.first_memory_chunk = NULL;
+    memory_manager.memory_start = heap;
+    return 0;
+}
--- heap.h
+++ heap.h
@@ -1,4 +1,29 @@
 #ifndef HEAP_PROJECT_TO_GIT_HEAP_H
 #define HEAP_PROJECT_TO_GIT_HEAP_H
 
+#include <stddef.h>
+#include <inttypes.h>
+#include <string.h>
+#include <unistd.h>
+#define FENCE_SIZE 8
+
+struct memory_manager_t
+{
+    void *memory_start;
+    size_t memory_size;
+    struct memory_chunk_t *first_memory_chunk;
+};
+
+struct memory_chunk_t
+{
+    struct memory_chunk_t* prev;
+    struct memory_chunk_t* next;
+    size_t size;
+    int free;
+    int hash;
+};
+
+int heap_setup(void);
+void heap_clean(void);
+
 #endif
